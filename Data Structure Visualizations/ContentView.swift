97eacc694ade0bd134f79fc1a74bfc2c583d3e99
wttch（王冲） <wttch@wttch.com>
2022-04-14 11:54:38 +0800

    Initial Commit

---
+++ Data Structure Visualizations/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Data Structure Visualizations
+//
+//  Created by Wttch on 2022/4/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Data Structure Visualizations/Data_Structure_VisualizationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Data_Structure_VisualizationsApp.swift
+//  Data Structure Visualizations
+//
+//  Created by Wttch on 2022/4/14.
+//
+
+import SwiftUI
+
+@main
+struct Data_Structure_VisualizationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
